50a9fe22aeb00ee5cde8c9ac3dff7b7045c140e4
Alejandro-Sedano <alejandrosedano85@gmail.com>
2022-09-14 02:21:10 -0500

    a

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express');
+var express = require('express');
+var socket = require('socket.io');
 
-//App setup
-const app = express();
-const server = app.listen(4000, function(){
-    console.log('listening to requests on port 4000');
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
 });
 
-//Static files
+// Static files
 app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
